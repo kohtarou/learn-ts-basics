ce1199ff0244c1e2d92c4f15ae164fdf34281fdd
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-10-05 18:22:37 +0900

    first commit

---
+++ src/prac01.ts
@@ -0,0 +1,9 @@
+export {};
+//const userName: string = "Bob";
+//console.log(`Hey, ${userName}.`);
+
+// 文字列型 (string type) の変数 name の宣言と初期化
+let name: string = "TypeScriotの勉強";
+// 数値型 (number type) の変数 priority の宣言と初期化
+let priority: number = 3;
+console.log(name, priority);
