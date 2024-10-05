da2157569f1731c9c370ff2ec51e66fc4167fa48
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-10-05 18:28:00 +0900

    8.3

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,9 +1,20 @@
 export {};
-//const userName: string = "Bob";
-//console.log(`Hey, ${userName}.`);
+const name: string = "TypeScriptの勉強";
+const priority: number = 3;
+console.log(`Todo 1 => ${name}（優先度:${priority})`); // テンプレート文字列
+/*
+// Date型の変数 deadline の宣言と初期化
+let deadline: Date = new Date(2024, 10, 2, 11, 45);
 
-// 文字列型 (string type) の変数 name の宣言と初期化
-let name: string = "TypeScriotの勉強";
-// 数値型 (number type) の変数 priority の宣言と初期化
-let priority: number = 3;
-console.log(name, priority);
+
+// 年、月、日、時間、分を取得
+let year = deadline.getFullYear();
+let month = (deadline.getMonth() + 1).toString().padStart(2, '0'); // 月は0から始まるので+1
+let day = deadline.getDate().toString().padStart(2, '0');
+let hours = deadline.getHours().toString().padStart(2, '0');
+let minutes = deadline.getMinutes().toString().padStart(2, '0');
+
+// フォーマットされた文字列を作成
+let formattedDate = `${year}/${month}/${day} ${hours}:${minutes}`;
+console.log(formattedDate); // "2024/11/02 11:45" の形式で出力される
+*/
