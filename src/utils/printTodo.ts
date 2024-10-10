3ef2a801bea0a8a120ec2d96faa2005590ab1b33
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-10-10 10:31:03 +0900

    wqf

--- src/utils/printTodo.ts
+++ src/utils/printTodo.ts
@@ -2,8 +2,20 @@ import dayjs from "dayjs";
 import { Todo } from "../types";
 
 export const printTodo = (todo: Todo): void => {
+  const { name, priority, deadline } = todo; // 分割代入
   const todoSummary =
     `(優先度: ${todo.priority}) ${todo.name}` +
     ` 期日: ${dayjs(todo.deadline).format("YYYY/MM/DD HH:mm")}`;
   console.log(todoSummary);
 };
+/*
+export const printTodo = (todo: Todo): void => {
+    const name = todo.name;          // 通常の代入
+    const priority = todo.priority;  // 通常の代入
+    const deadline = todo.deadline;  // 通常の代入
+    const todoSummary =
+      `(優先度: ${priority}) ${name}` +
+      ` 期日: ${dayjs(deadline).format("YYYY/MM/DD HH:mm")}`;
+    console.log(todoSummary);
+  };
+*/
